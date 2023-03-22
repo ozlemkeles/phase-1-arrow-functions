24f19bb09b88737125103b1d44881a13ff15e6e9
Özlem Keleş <ozlemkeles98@gmail.com>
2023-03-22 23:30:40 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+// Code
+
+function divide(){
+    const a = 2000;
+    const b = 100;
+    return a / b;
+}
+
+square = (number) => Math.pow(number, number);
+
+add = (number1, number2) => number1 + number2;
